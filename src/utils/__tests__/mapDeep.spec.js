831d3e3eb0282bf8e5ecd2fafca158e61e7c6260
oreqizer <oreqizer@gmail.com>
2016-09-10 09:33:57 +0200

    mapDeep: Simplify function

--- src/utils/__tests__/mapDeep.spec.js
+++ src/utils/__tests__/mapDeep.spec.js
@@ -9,15 +9,15 @@ jest.unmock('../../containers/FieldStore');
 jest.unmock('ramda');
 
 const flat = {
-  field1: { value: 'test' },
-  field2: { value: '1337' },
+  field1: new FieldStore(),
+  field2: new FieldStore(),
 };
 
 const medium = {
-  field1: { value: 'test' },
+  field1: new FieldStore(),
   fieldArray: [
-    { field: { value: 'medium0' } },
-    { field: { value: 'medium1' } },
+    { field: new FieldStore() },
+    { field: new FieldStore() },
   ],
   flatArray: [
     new FieldStore(),
@@ -26,11 +26,11 @@ const medium = {
 };
 
 const deep = {
-  field1: { value: 'test' },
+  field1: new FieldStore(),
   fieldArray: [
-    { field: { value: 'medium0' } },
+    { field: new FieldStore() },
     {
-      field: { value: 'medium1' },
+      field: new FieldStore(),
       nested: [
         new FieldStore(),
       ],
@@ -42,16 +42,16 @@ describe('#mapDeep', () => {
   it('should map a flat form', () => {
     const result = mapDeep(R.prop('value'), flat);
 
-    expect(result.field1).toBe('test');
-    expect(result.field2).toBe('1337');
+    expect(result.field1).toBe('');
+    expect(result.field2).toBe('');
   });
 
   it('should map a medium form', () => {
     const result = mapDeep(R.prop('value'), medium);
 
-    expect(result.field1).toBe('test');
-    expect(result.fieldArray[0].field).toBe('medium0');
-    expect(result.fieldArray[1].field).toBe('medium1');
+    expect(result.field1).toBe('');
+    expect(result.fieldArray[0].field).toBe('');
+    expect(result.fieldArray[1].field).toBe('');
     expect(result.flatArray[0]).toBe('');
     expect(result.flatArray[1]).toBe('');
   });
@@ -59,9 +59,9 @@ describe('#mapDeep', () => {
   it('should map a deep form', () => {
     const result = mapDeep(R.prop('value'), deep);
 
-    expect(result.field1).toBe('test');
-    expect(result.fieldArray[0].field).toBe('medium0');
-    expect(result.fieldArray[1].field).toBe('medium1');
+    expect(result.field1).toBe('');
+    expect(result.fieldArray[0].field).toBe('');
+    expect(result.fieldArray[1].field).toBe('');
     expect(result.fieldArray[1].nested[0]).toBe('');
   });
 });
--- src/utils/mapDeep.js
+++ src/utils/mapDeep.js
@@ -3,14 +3,9 @@ import R from 'ramda';
 
 import FieldStore from '../containers/FieldStore';
 
-const mapArray = (fn, objfn, val) =>
+const mapDeep = (fn, form) => R.map(val =>
   val instanceof FieldStore ?
   fn(val) :
-  objfn(fn, val);
-
-const mapDeep = (fn, form) => R.map(val =>
-  R.isArrayLike(val) ?
-  R.map(R.partial(mapArray, [fn, mapDeep]), val) :
-  fn(val), form);
+  mapDeep(fn, val), form);
 
 export default mapDeep;
