6204eb525f9efe999be0b2fd9726851267f6636c
oreqizer <oreqizer@gmail.com>
2016-10-04 17:06:36 +0200

    index: Export FieldArray

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Field from './Field';
+import FieldArray from './FieldArray';
 import FormsStore from './FormsStore';
 import mobxForm from './mobxForm';
 
 export {
   Field,
+  FieldArray,
   FormsStore,
   mobxForm,
 };
