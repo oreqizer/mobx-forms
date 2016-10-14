bdd26c0d63652275e81a5d7957b3d063f3e2def9
oreqizer <oreqizer@gmail.com>
2016-10-14 11:40:37 +0200

    typescript: Adjust build 2

--- jest-preprocessor.js
+++ jest-preprocessor.js
@@ -1,9 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const ts = require('typescript');
 
 module.exports = {
   process(src, path) {
     if (path.endsWith('.ts') || path.endsWith('.tsx')) {
       const opts = {
+        target: ts.ScriptTarget.ES6,
         module: ts.ModuleKind.CommonJS,
         jsx: ts.JsxEmit.React,
       };
